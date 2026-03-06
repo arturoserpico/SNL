1c3f1e87b5463ddf938d380816b3bec89d565a84
artur <arturo.serpico@gmail.com>
2026-03-06 21:29:23 +0100

    e

--- SNL/src/Symbolic/SymOperators.h
+++ SNL/src/Symbolic/SymOperators.h
@@ -4,30 +4,6 @@
 #include "Function.h"
 
 namespace snl {
-	auto binOperatorsWrapper(IsSym auto& a, IsSym auto& b) {
-		using A = RemSym<decltype(a)>;
-		using B = RemSym<decltype(b)>;
-		return std::tuple<Ref<Sym<A>>, Ref<Sym<B>>>{ a, b };
-	}
-
-	auto binOperatorsWrapper(IsSym auto& a, const IsSym auto& b) {
-		using A = RemSym<decltype(a)>;
-		using B = RemSym<decltype(b)>;
-		return std::tuple<Ref<Sym<A>>, Ref<Sym<B>>>{ a, makeManaged<Sym<B>>(b) };
-	}
-
-	auto binOperatorsWrapper(const IsSym auto& a, IsSym auto& b) {
-		using A = RemSym<decltype(a)>;
-		using B = RemSym<decltype(b)>;
-		return std::tuple<Ref<Sym<A>>, Ref<Sym<B>>>{ makeManaged<Sym<A>>(a), b };
-	}
-
-	auto binOperatorsWrapper(const IsSym auto& a, const IsSym auto& b) {
-		using A = RemSym<decltype(a)>;
-		using B = RemSym<decltype(b)>;
-		return std::tuple<Ref<Sym<A>>, Ref<Sym<B>>>{ makeManaged<Sym<A>>(a), makeManaged<Sym<B>>(b) };
-	}
-
 	auto operator+(auto&& _a, auto&& _b) {
 		auto [a, b] = convertArgsToSymRef(std::forward<decltype(_a)>(_a), std::forward<decltype(_b)>(_b));
 		using A = RemSym<RemRef<decltype(a)>>;
