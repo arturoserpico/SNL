968c13dc4854acdd7fef2514efbbe27567d838bf
Arturo Serpico <Arturo Serpico@DESKTOP-LH6J71A>
2026-06-24 02:46:53 +0200

    e

--- SNL/src/Main.cpp
+++ SNL/src/Main.cpp
@@ -22,13 +22,16 @@ int main() {
 	using namespace snl::literals;
 
 	auto x = snl::matchVar<snl::Nat>();
+	auto y = snl::matchVar<snl::Nat>();
 	
 	snl::Set set(snl::nat * snl::nat);
 
 	set = { (x, snl::pow(x, 2)) };
 	
 	std::cout
-		<< set.contains((1_nat, 1_nat)) << " "
-		<< set.contains((2_nat, 4_nat)) << " "
-		<< set.contains((3_nat, 9_nat));
+		<< set.contains((1_nat, 1_nat)) << " " //true 1^2 == 1
+		<< set.contains((2_nat, 4_nat)) << " " //true 2^2 == 4 
+		<< set.contains((2_nat, 5_nat)) << " " //false
+		<< set.contains((3_nat, 9_nat)) << " " //true 3^2 == 9
+		<< set.contains((7_nat, y)); //true y binds to 49
 }
